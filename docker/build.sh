b0034593e7ee5d76cdede6d5533a3225170edfff
Javier Arribas <javiarribas@gmail.com>
2021-12-24 16:42:51 +0100

    Fix build gnss-sdr unit tests

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@
 
 docker build -t gnuradio-android .
 
-docker run -it --privileged --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/home/android/.Xauthority:rw"  gnuradio-android
+#docker run -it --privileged --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/home/android/.Xauthority:rw"  gnuradio-android
